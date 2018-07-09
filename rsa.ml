f4525eadd04a6ef1b20d4d4f1cf837b845cb6d5a
Vivien PELLETIER <vivien.pelletier@gmail.com>
2018-07-09 18:04:48 +0200

    comments and fun notation

--- rsa.ml
+++ rsa.ml
@@ -11,37 +11,45 @@ type public_key = {
 
 type crypted_msg = bytes list
 
+(* init random *)
+let _ = Random.self_init()
+
+(* prime number generation *)
 let primes_list = 
-  let select x =
+  let select = fun x ->
     gt_big_int (of_int x) (pow (of_int 2) (of_int 13)) in
   List.filter select (Primes.primes (BatInt.pow 2 14))
 let primes_count = List.length primes_list
 
-let random_prime () =
+let random_prime = fun () ->
   let i  = Random.int primes_count in
   of_int (List.nth primes_list i)
 
-let euclid phi_n e =
-  let rec eucl r u v r2 u2 v2 = 
+(* extended euclidian algorithm :
+ * return (gcd(phi_n, e), invert of phi_n, invert of e) *)
+let euclid = fun phi_n e ->
+  let rec euclid = fun r u v r2 u2 v2 ->
     if equal r2 zero then (r, u, v)
-    else eucl r2 u2 v2 (r - (r/r2)*r2) (u - (r/r2)*u2) (v - (r/r2)*v2) in
-  eucl phi_n one zero e zero one
+    else euclid r2 u2 v2 (r - (r/r2)*r2) (u - (r/r2)*u2) (v - (r/r2)*v2) in
+  euclid phi_n one zero e zero one
 
-let phi p q = 
+(* return (p-1)*(q-1) *)
+let phi = fun p q ->
   let a = pred p and b = pred q in
   let (gcd,_,_) = euclid a b in
   a*b/gcd
 
-
-let _ = Random.self_init()
-
-let rec generate_keys () =
+(* generate duo of key
+ * return (private key, public key) *)
+(* TODO parametized key size *)
+(* TODO choice of stronger integer *)
+let rec generate_keys = fun () ->
   let e   = of_int (Random.int 10000)
   and p   = random_prime()
   and q   = random_prime() in
   let n   = p*q
   and phi = phi p q in
-  let (r, u, v) = euclid phi e in
+  let (r, _, v) = euclid phi e in
   if equal r one then ({
       p   = p;
       q   = q;
@@ -52,21 +60,26 @@ let rec generate_keys () =
   else
     generate_keys()
 
-let powmod x e m =
-  let rec powmod (x, e, m, acc) = 
+(* compute x^e [m] *)
+(* TODO replace by lower complexity algorithm *)
+let powmod = fun x e m ->
+  let rec powmod = fun x e m acc ->
     if equal e zero
     then acc
-    else powmod (x, pred e, m, mod_big_int (acc*x) m) in
-  powmod (x, e, m, one)
+    else powmod x (pred e) m (mod_big_int (acc*x) m) in
+  powmod x e m one
 
-let encrypt_big_int k m = 
+(* encrypt big_integer to big integer *)
+let encrypt_big_int = fun  k m ->
   powmod m k.e k.n
 
-let decrypt_big_int k m = 
+(* decrypt big integer to big integer *)
+let decrypt_big_int = fun  k m ->
   powmod m k.d (k.p*k.q)
 
-let bytes_of_big_int big_int2conv = 
-  let rec bytes_of_big_int2 big_int2conv bytes_converted =
+(* convert big integer to bytes *)
+let bytes_of_big_int = fun big_int2conv ->
+  let rec bytes_of_big_int = fun big_int2conv bytes_converted ->
     if lt_big_int big_int2conv (of_int 255) then
       Bytes.cat 
         bytes_converted
@@ -74,7 +87,7 @@ let bytes_of_big_int big_int2conv =
            (char_of_int 
               (int_of_big_int big_int2conv)))
     else
-      bytes_of_big_int2 
+      bytes_of_big_int 
         (big_int2conv / (of_int 256))
         (Bytes.cat 
            bytes_converted
@@ -83,24 +96,26 @@ let bytes_of_big_int big_int2conv =
                  (int_of_big_int
                     (mod_big_int big_int2conv (of_int 256))))))
   in
-  bytes_of_big_int2 big_int2conv (Bytes.create 0)
+  bytes_of_big_int big_int2conv (Bytes.create 0)
 
-let big_int_of_bytes bytes2conv =
-  let rec big_int_of_bytes2 bytes2conv converted_big_int =
+(* convert bytes to big integer *)
+let big_int_of_bytes = fun bytes2conv ->
+  let rec big_int_of_bytes = fun bytes2conv converted_big_int ->
     if Bytes.length bytes2conv = 0 then
       converted_big_int
     else
-      big_int_of_bytes2 
+      big_int_of_bytes
         (Bytes.sub bytes2conv 1 
            (Pervasives.pred
               (Bytes.length bytes2conv)))
         converted_big_int*(of_int 256) + (of_int(int_of_char (Bytes.get bytes2conv 0)))
   in
-  big_int_of_bytes2 bytes2conv zero
+  big_int_of_bytes bytes2conv zero
 
-let encrypt key bytes2encrypt = 
+(* encrypt with a public key a message in bytes *)
+let encrypt = fun key bytes2encrypt ->
   let block_bytes_size = Pervasives.(/) (num_bits_big_int key.n) 8 in
-  let rec encrypt_blocks begin_index encrypted_blocks =
+  let rec encrypt begin_index encrypted_blocks =
     let encrypt_one_block = fun end_index ->
       bytes_of_big_int 
         (encrypt_big_int key 
@@ -112,14 +127,15 @@ let encrypt key bytes2encrypt =
         (Pervasives.(-) (Bytes.length bytes2encrypt) begin_index)
       :: encrypted_blocks
     else 
-      encrypt_blocks 
+      encrypt 
         (Pervasives.(+)  begin_index block_bytes_size) 
         ((encrypt_one_block block_bytes_size)::encrypted_blocks)
   in
-  encrypt_blocks 0 []
+  encrypt 0 []
 
+(* decrypt with a private key a crypted message to bytes *)
 let decrypt = fun key blocks2decrypt ->
-  let rec decrypt_blocks = fun blocks2decrypt decrypted_bytes ->
+  let rec decrypt = fun blocks2decrypt decrypted_bytes ->
     let decrypt_one_block = fun block ->
       bytes_of_big_int 
         (decrypt_big_int key 
@@ -127,6 +143,6 @@ let decrypt = fun key blocks2decrypt ->
     in
     match blocks2decrypt with
     | [] -> decrypted_bytes
-    | head::tail -> decrypt_blocks tail (Bytes.cat (decrypt_one_block head) decrypted_bytes)
+    | head::tail -> decrypt tail (Bytes.cat (decrypt_one_block head) decrypted_bytes)
   in
-  decrypt_blocks blocks2decrypt (Bytes.create 0)
+  decrypt blocks2decrypt (Bytes.create 0)
